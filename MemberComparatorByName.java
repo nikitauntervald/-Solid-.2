1f8550961bd2437339f6a7f549e7469ffa4d7547
NikitaUnterwald <60911883+nikitauntervald@users.noreply.github.com>
2024-06-11 08:10:42 +0300

    Add files via upload

---
+++ MemberComparatorByName.java
@@ -0,0 +1,19 @@
+package familyTree.comparators;
+import familyTree.member.Human;
+import java.util.Comparator;
+public class MemberComparatorByName implements Comparator<Human> {
+    @Override
+    public int compare(Human o1, Human o2) {
+        return o1.getName().compareTo(o2.getName());
+    }
+
+    //    @Override
+//    public int compare(Human o1, Human o2) {
+//        int result = o1.getSurname().compareTo(o2.getSurname());
+//        if (result == 0) {
+//            result = o1.getName().compareTo(o2.getName());
+//        }
+//        return result;
+//    }
+
+}
